5e07ebc5c70b5e7b6291d4d06dff4529cd46a7bf
ROBIUL ISLAM <r_islam@live.com>
2018-09-25 03:04:43 +0300

    1

--- root/settings.py
+++ root/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import  django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,6 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'DATABASE_URL' : django_heroku.settings(locals()),
     }
 }
 
